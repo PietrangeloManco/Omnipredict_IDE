da322fe53a1c74943bbdc644f48b91f7eb56e0e3
Pietrangelo Manco <p.manco@studenti.unipi.it>
2025-05-26 15:46:18 +0200

    test

--- omnipredict_ide/settings.py
+++ omnipredict_ide/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '***REMOVED***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['omnipredict.onrender.com']
+ALLOWED_HOSTS = ['omnipredict-ide.onrender.com']
 
 
 # Application definition
