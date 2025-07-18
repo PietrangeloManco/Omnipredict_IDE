6b385d6ec99df805d302fcbe8e8e396abafccce8
PietrangeloManco <p.manco@studenti.unipi.it>
2025-07-18 16:04:16 +0000

    Fixed static files configuration and added logo

--- omnipredict_ide/settings.py
+++ omnipredict_ide/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '***REMOVED***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['omnipredict-ide.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['omnipredict.it', 'www.omnipredict.it','omnipredict-ide.onrender.com', '127.0.0.1']
 
 
 # Application definition
@@ -117,9 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [ BASE_DIR / "static" ]
+# settings.py
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -129,3 +132,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/'
 LOGIN_REDIRECT_URL = "/upload/"      # or wherever you want to go after login
 LOGOUT_REDIRECT_URL = "/"
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://omnipredict.it',
+'https://www.omnipredict.it'
+]
+
---
+++ settings.py
@@ -0,0 +1,15 @@
+
+# Static files configuration
+STATIC_ROOT = '/var/www/omnipredict.it/static/'
+STATIC_URL = '/static/'
+
+
+# Static files configuration
+STATIC_ROOT = '/var/www/omnipredict.it/static/'
+STATIC_URL = '/static/'
+
+
+# Static files configuration
+STATIC_ROOT = '/var/www/omnipredict.it/static/'
+STATIC_URL = '/static/'
+
